ed0b00828b18aefc5e3a276f50d78502d05441ad
derusc <derusc@deruschi.fake>
2013-11-08 14:40:55 +0100

    Main method does something.

--- src/main/java/com/deruschi/tutorial/tdd/coffemachine/App.java
+++ src/main/java/com/deruschi/tutorial/tdd/coffemachine/App.java
@@ -1,5 +1,9 @@
 package com.deruschi.tutorial.tdd.coffemachine;
 
+import com.deruschi.tutorial.tddcoffemachine.exception.NegativeAmountException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Test Driven Development Example" );
+        try {
+            ConsumerOrder order = new ConsumerOrder(Drink.Coffe, 0, 4, true);
+        } catch (NegativeAmountException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
